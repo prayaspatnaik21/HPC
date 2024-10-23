5d6c40e44be24a895c2906dff0daeba7f735b48a
prayaspatnaik21 <prayaspatnaik21@gmail.com>
2024-10-22 20:35:36 -0500

    Platform Extension

--- src/Host_Programming.c
+++ src/Host_Programming.c
@@ -50,6 +50,58 @@ void platform_extension_test()
     cl_int i , err , platform_index = -1;
 
     char* ext_data;
+    size_t ext_size;
+    const char icd_ext[] = "cl_khr_icd";
+
+    // Find number of Platforms
+    err = clGetPlatformIDs(1 , NULL , &num_platforms);
+    if(err < 0)
+    {
+        perror("Couldn't find any platforms");
+        exit(1);
+    }
+
+    // Allocate platform array
+    platforms = (cl_platform_id*) malloc(sizeof(cl_platform_id) * num_platforms);
+
+    // Initialize platform array
+    clGetPlatformIDs(num_platforms, platforms, NULL);
+
+    // Find size of Extension Data
+    for(i = 0  ; i < num_platforms ; i++)
+    {
+        err = clGetPlatformInfo(platforms[i] , CL_PLATFORM_EXTENSIONS , 0 , NULL , &ext_size);
+        if(err < 0)
+        {
+            perror("Couldn't read extension data");
+            exit(1);
+        }
+
+        // Read Extension data
+        ext_data = (char*)malloc(ext_size);
+        clGetPlatformInfo(platforms[i] , CL_PLATFORM_EXTENSIONS, ext_size , ext_data , NULL);
+        printf("Platform %d supports extensions: %s\n", i , ext_data);
+
+        if(strstr(ext_data , icd_ext) != NULL)
+        {
+            free(ext_data);
+            platform_index = i;
+            break;
+        }
+        
+        free(ext_data);
+    }
+
+    if(platform_index > -1)
+    {
+        printf("Platform %d supports the %s extension. \n", platform_index , icd_ext);
+    }
+    else
+    {
+        printf("No Platforms support the %s extension. \n", icd_ext);
+    }
+
+    free(platforms);
 }
 int main()
 {
